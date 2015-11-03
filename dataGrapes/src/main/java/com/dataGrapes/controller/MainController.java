aeaa5de4010ef9f89cbd50754cbaa5c6eff0f5ae
bahamutr13 <tedlpascua@gmail.com>
2015-11-03 14:00:54 -0800

    Test
    abc

---
+++ dataGrapes/src/main/java/com/dataGrapes/controller/MainController.java
@@ -0,0 +1,43 @@
+package com.dataGrapes.controller;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+    private static DataSource dataSource;
+    private static JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String goHome(ModelMap model) {
+        return "/WEB-INF/index.jsp";
+    }
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        System.out.println("setting jdbc template");
+    }
+
+    public static JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+
+    public void listHotels(){
+        int rowCount = this.jdbcTemplate.queryForObject("select count(*) from HOTEL", Integer.class);
+        System.out.println(rowCount);
+    }
+
+}
